066ccaceafc1dc08b892c715a7061067fdf677ce
rikkertdebaas <Richard.felius@gmail.com>
2018-04-03 10:08:47 +0200

    capistrano update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,14 @@
-# config valid for current version and patch releases of Capistrano
+
 lock '3.6.1'
 
-set :application, "websiterails"
-set :repo_url, "git@example.com:me/my_repo.git"
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
+set :application, 'websiterails'
+set :repo_url, 'git@github.com:rikkertdebaas/websiterails.git'
+set :rvm_ruby_version, '2.3.3'
+set :passenger_restart_with_touch, true
+set :pty, true
+set :ssh_options, forward_agent: true
+append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
 
@@ -17,8 +19,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # These are the defaults.
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
-# Default value for :pty is false
-# set :pty, true
+
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
